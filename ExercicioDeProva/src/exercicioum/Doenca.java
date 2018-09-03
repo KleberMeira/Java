4a3f59fac2fe844590225ab32c9e9c40b10bbc9a
KleberMeira <39809072+KleberMeira@users.noreply.github.com>
2018-09-03 15:00:41 -0400

    Elaboração de Dois Sistemas de Gerenciamento
    1- Sistema de Gerenciamento para Posto de Saúde visando a atividade do Enfermeiro.
    2 - Sistema ficticio para gerenciamento de informações basicas de Vereadores, Deputado Estadual e Federal.

---
+++ ExercicioDeProva/src/exercicioum/Doenca.java
@@ -0,0 +1,5 @@
+package exercicioum;
+
+public class Doenca {
+
+}
---
+++ ExercicioDeProva/src/exercicioum/Teste.java
@@ -0,0 +1,12 @@
+package exercicioum;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Paciente a = new Paciente("Kleber", 0773731350);
+		System.out.println(a.toString());
+	}
+
+}
