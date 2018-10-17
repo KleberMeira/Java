b01e19d894e372b6c19c78987739746bc156af5b
KleberMeira <klebermeira4@gmail.com>
2018-10-17 13:35:05 -0400

    pasta

---
+++ Lista_Lineares/ListasLineares/ListaLinearInfinita.java
@@ -0,0 +1,15 @@
+package ListasLineares;
+
+public interface ListaLinearInfinita {
+
+	void add(int e);
+	void add(int index, int elemento) throws IndexOutOfBoundsException;
+	void clear();
+	void contains(int elemento);
+	boolean equals(ListaLinearInfinita lista);
+	int get(int index) throws IndexOutOfBoundsException;
+	boolean remove(int elemento) throws IndexOutOfBoundsException;
+	int set(int posicao, int elemento) throws IndexOutOfBoundsException;
+	int size();
+	
+}
