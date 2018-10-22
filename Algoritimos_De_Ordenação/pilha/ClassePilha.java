b73fe7052cd54a026b65ff9a135ff327304fd634
KleberMeira <klebermeira4@gmail.com>
2018-10-22 14:39:22 -0400

    Adciona: Arquivos Fila e Pilha e contrução de Fila

---
+++ Algoritimos_De_Ordenação/pilha/ClassePilha.java
@@ -0,0 +1,49 @@
+package pilha;
+
+public class ClassePilha {
+
+	private int tamanho = 0;
+	private int elemento = 0;
+	int[] vetor;
+	
+	public ClassePilha(int tamanho) {
+		
+		this.tamanho = tamanho;
+		this.elemento = elemento;
+		vetor = new int[tamanho];
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+	
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public int getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(int elemento) {
+		this.elemento = elemento;
+	}
+
+	
+	/////PUSH -- EMPILHA
+	public void push(int elemento) {
+	
+		
+		
+	}
+	
+	/////POP -- DESEMPILHA
+	
+	/////VERIFICA O TOPO
+	
+	/////IMPRESSÃO
+
+	
+	
+	
+}
---
+++ Algoritimos_De_Ordenação/pilha/TestePilha.java
@@ -0,0 +1,10 @@
+package pilha;
+
+public class TestePilha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
