e8363c8d58623eb55f26240afa04fd93524a1a5a
KleberMeira <klebermeira4@gmail.com>
2018-10-30 22:52:24 -0400

    Adciona: Implementação em tratamento de excecoes

---
+++ tratamento_de_excecoes/p2_lpoo_2016_02/CadastroDePessoas.java
@@ -0,0 +1,14 @@
+package p2_lpoo_2016_02;
+
+public class CadastroDePessoas extends Pessoa {
+
+	public CadastroDePessoas(String nome, String cpf, String rg) {
+		super(nome, cpf, rg);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void IncluirPessoa() {
+		
+	}
+
+}
