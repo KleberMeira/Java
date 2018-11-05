0c6f9fbd0eedb8d9a923574c0c2d83847930d0fb
Kleber Meira Lima <201719061173@lab01-l25.dominio.facom.ufms.br>
2018-11-05 15:37:22 -0300

    Implementacao: pilha -infinita-

--- listas_lineares/p1_as_2016/TestePilha.java
+++ listas_lineares/p1_as_2016/TestePilha.java
@@ -1,15 +1,23 @@
 public class TestePilha{
     public static void main(String[] args) {
-    
-        Pilha teste = new Pilha(4, 1);
+	    Pilha teste = new Pilha(4, 1);
+	    
+	    teste.empilha(7);
+	    teste.empilha(6);
+	    teste.empilha(5);
+	    teste.empilha(4);
 		teste.empilha(3);
 		teste.empilha(2);
 		teste.empilha(1);
+		
+		System.out.println(teste.isEmpty()); //Momento em que NÂO está vazia
 		
 		while(!teste.isEmpty()) {
 			int elemento = teste.desempilha();
 			System.out.println("Desempilhei: " + elemento);
 		}
+		
+		System.out.println(teste.isEmpty()); //Momento que está vazia
 
     }
 }
